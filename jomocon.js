244a2b21c87d9071f6104b03730ce5076fff9c88
mattfussell <matt.fussell@gmail.com>
2021-05-20 22:50:26 -0500

    slight change in visual direction: new lead image, new lead fonts, change in logo layout; nav has basic styles applied; landing page will be solo page for now because we need something launched

--- jomocon.js
+++ jomocon.js
@@ -2,7 +2,10 @@
 
 // selections
 const navToggle = document.querySelector('.close-nav'),
-      navLinks = document.querySelectorAll('.navLink')
+      navLinks = document.querySelectorAll('.navLink'),
+      navHomeText = document.querySelector('.homeText'),
+      navSocials = document.querySelector('.navSocials'),
+      navElementContainer = document.querySelector('.navContainer')
 
 // functions
 const init = function() {
@@ -18,11 +21,19 @@ const toggleNavState = function(e) {
     navLinks.forEach(link => {
       link.classList.add('hidden')
     })
+    navHomeText.classList.remove('hidden')
+    navToggle.classList.remove('rotate45')
+    navSocials.classList.remove('navSocialsH')
+    navElementContainer.classList.remove('navContainerOpen')
   } else {
     e.target.parentElement.parentElement.classList.add('openNav')
     navLinks.forEach(link => {
       link.classList.remove('hidden')
     })
+    navHomeText.classList.add('hidden')
+    navToggle.classList.add('rotate45')
+    navSocials.classList.add('navSocialsH')
+    navElementContainer.classList.add('navContainerOpen')
   }
 }
 
