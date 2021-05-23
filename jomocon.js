68928835cf01e0226bdd64de1b7b5789a8e1387a
mattfussell <matt.fussell@gmail.com>
2021-05-22 20:16:23 -0500

    contact area tidied up; _about us_ card functionality implemented - still needs to be styled

--- jomocon.js
+++ jomocon.js
@@ -5,17 +5,25 @@ const navToggle = document.querySelector('.close-nav'),
       navLinks = document.querySelectorAll('.navLink'),
       navHomeText = document.querySelector('.homeText'),
       navSocials = document.querySelector('.navSocials'),
-      navElementContainer = document.querySelector('.navContainer')
+      navElementContainer = document.querySelector('.navContainer'),
+      pageDate = document.querySelector('.currentYear'),
+      aboutCards = document.querySelectorAll('.card'),
+      aboutCardsArea = document.querySelector('.teamCards'),
+      aboutCardsInfo = document.querySelector('.cardAbout')
 
 // functions
 const init = function() {
-
+  setCurrentYear()
 }
+
+/* Set current year */
+const setCurrentYear = _ => pageDate.innerText = new Date().getFullYear()
 
 /* open or close navigation */
 const toggleNavState = function(e) {
   e.preventDefault()
 
+  // close the nav
   if (e.target.parentElement.parentElement.classList.contains('openNav')) {
     e.target.parentElement.parentElement.classList.remove('openNav')
     navLinks.forEach(link => {
@@ -25,7 +33,7 @@ const toggleNavState = function(e) {
     navToggle.classList.remove('rotate45')
     navSocials.classList.remove('navSocialsH')
     navElementContainer.classList.remove('navContainerOpen')
-  } else {
+  } else { // open the nav
     e.target.parentElement.parentElement.classList.add('openNav')
     navLinks.forEach(link => {
       link.classList.remove('hidden')
@@ -37,10 +45,67 @@ const toggleNavState = function(e) {
   }
 }
 
+// BUG: clicking on the about text removes the entire card instead of shrinking it back to 
+/* expand or contract team member card */
+const toggleAboutCardState = function(e) {
+  // handle direct card clicks
+  const handleParent = function(e) {
+    // close the card
+    if (e.target.classList.contains('openCard')) {
+      e.target.lastElementChild.classList.add('hidden')
+      e.target.classList.remove('openCard')
+
+      aboutCards.forEach(card => {
+        if (card.classList.contains('hidden')) {
+          card.classList.remove('hidden')
+        }
+      })
+    } else { // open the card
+      aboutCards.forEach(card => {
+        if (card.dataset.id !== e.target.dataset.id) {
+          card.classList.add('hidden')
+        }
+      })
+
+      e.target.classList.add('openCard')
+      e.target.lastElementChild.classList.remove('hidden')
+    }
+  }
+
+  // handle child element clicks
+  const handleChild = function(e) {
+    if (e.target.parentElement.classList.contains('openCard')) {
+      e.target.parentElement.lastElementChild.classList.add('hidden')
+      e.target.parentElement.classList.remove('openCard')
+
+      aboutCards.forEach(card => {
+        if (card.classList.contains('hidden')) {
+          card.classList.remove('hidden')
+        }
+      })
+    } else { // open the card
+      aboutCards.forEach(card => {
+        if (card.dataset.id !== e.target.parentElement.dataset.id) {
+          card.classList.add('hidden')
+        }
+      })
+
+      e.target.parentElement.classList.add('openCard')
+      e.target.parentElement.lastElementChild.classList.remove('hidden')
+    }
+  }
+  
+  // isolate source, call proper function
+  if (e.target.classList.contains('card')) {
+    handleParent(e)
+  } else if (e.target.parentElement.classList.contains('card')) {
+    handleChild(e)
+  }
+}
+
 // event listeners
-navToggle.addEventListener('click', e => {
-  toggleNavState(e)
-})
+navToggle.addEventListener('click', e => toggleNavState(e))
+aboutCardsArea.addEventListener('click', e => toggleAboutCardState(e))
 
 // init page JS
 init()
