6293d8425f8517061a3b70a995bb1790cf84c44c
mattfussell <matt.fussell@gmail.com>
2021-05-30 21:09:45 -0500

    All of the requisite content for launch is added to the site. The nav and bio animations are working at an acceptable level to get the site launched. The next step is responsive scaling adjustments cross-browser compatiblity

--- jomocon.js
+++ jomocon.js
@@ -9,7 +9,8 @@ const navToggle = document.querySelector('.close-nav'),
       pageDate = document.querySelector('.currentYear'),
       aboutCards = document.querySelectorAll('.card'),
       aboutCardsArea = document.querySelector('.teamCards'),
-      aboutCardsInfo = document.querySelector('.cardAbout')
+      aboutCardsInfo = document.querySelector('.cardAbout'),
+      navLinkContainer = document.querySelector('.navLinks') // LAUNCH: remove once site is fully implemented
 
 // functions
 const init = function() {
@@ -45,21 +46,34 @@ const toggleNavState = function(e) {
   }
 }
 
-// BUG: clicking on the about text removes the entire card instead of shrinking it back to 
+/* LAUNCH: handle under links in the nav - remove when site is fully built */
+const handleNavLink = function(e) {
+  if (e.target.classList.contains('comingSoon')) {
+    e.preventDefault()
+  }
+}
+
 /* expand or contract team member card */
 const toggleAboutCardState = function(e) {
   // handle direct card clicks
   const handleParent = function(e) {
     // close the card
     if (e.target.classList.contains('openCard')) {
-      e.target.lastElementChild.classList.add('hidden')
-      e.target.classList.remove('openCard')
+      e.target.lastElementChild.classList.remove('opFull')
+      setTimeout(function() {
+        e.target.lastElementChild.classList.add('hidden')
+        e.target.classList.remove('openCard')
+        e.target.classList.add('closedCard')
 
-      aboutCards.forEach(card => {
-        if (card.classList.contains('hidden')) {
-          card.classList.remove('hidden')
-        }
-      })
+        setTimeout(function() {
+          aboutCards.forEach(card => {
+            if (card.classList.contains('hidden')) {
+              card.classList.remove('hidden')
+            }
+          })
+        }, 1000)
+
+      }, 750)
     } else { // open the card
       aboutCards.forEach(card => {
         if (card.dataset.id !== e.target.dataset.id) {
@@ -67,22 +81,37 @@ const toggleAboutCardState = function(e) {
         }
       })
 
+      e.target.classList.remove('closedCard')
       e.target.classList.add('openCard')
-      e.target.lastElementChild.classList.remove('hidden')
+      setTimeout(function() {
+        e.target.lastElementChild.classList.remove('hidden')
+
+        setTimeout(function(){
+          e.target.lastElementChild.classList.add('opFull')
+        }, 250)
+      }, 750)
     }
   }
 
   // handle child element clicks
   const handleChild = function(e) {
+    // close the card
     if (e.target.parentElement.classList.contains('openCard')) {
-      e.target.parentElement.lastElementChild.classList.add('hidden')
-      e.target.parentElement.classList.remove('openCard')
+      e.target.parentElement.lastElementChild.classList.remove('opFull')
+      setTimeout(function() {
+        e.target.parentElement.lastElementChild.classList.add('hidden')
+        e.target.parentElement.classList.remove('openCard')
+        e.target.parentElement.classList.add('closedCard')
 
-      aboutCards.forEach(card => {
-        if (card.classList.contains('hidden')) {
-          card.classList.remove('hidden')
-        }
-      })
+        setTimeout(function() {
+          aboutCards.forEach(card => {
+            if (card.classList.contains('hidden')) {
+              card.classList.remove('hidden')
+            }
+          })
+        }, 1000)
+
+      }, 750)
     } else { // open the card
       aboutCards.forEach(card => {
         if (card.dataset.id !== e.target.parentElement.dataset.id) {
@@ -90,22 +119,70 @@ const toggleAboutCardState = function(e) {
         }
       })
 
+      e.target.parentElement.classList.remove('closedCard')
       e.target.parentElement.classList.add('openCard')
-      e.target.parentElement.lastElementChild.classList.remove('hidden')
+      setTimeout(function() {
+        e.target.parentElement.lastElementChild.classList.remove('hidden')
+
+        setTimeout(function(){
+          e.target.parentElement.lastElementChild.classList.add('opFull')
+        }, 250)
+      }, 750)
     }
   }
-  
+
+  // handle grandchild element clicks
+  const handleGrandchild = function(e) {
+    // close the card
+    if (e.target.parentElement.parentElement.classList.contains('openCard')) {
+      e.target.parentElement.parentElement.lastElementChild.classList.remove('opFull')
+      setTimeout(function() {
+        e.target.parentElement.parentElement.lastElementChild.classList.add('hidden')
+        e.target.parentElement.parentElement.classList.remove('openCard')
+        e.target.parentElement.parentElement.classList.add('closedCard')
+
+        setTimeout(function() {
+          aboutCards.forEach(card => {
+            if (card.classList.contains('hidden')) {
+              card.classList.remove('hidden')
+            }
+          })
+        }, 1000)
+
+      }, 750)
+    } else { // open the card
+      aboutCards.forEach(card => {
+        if (card.dataset.id !== e.target.parentElement.parentElement.dataset.id) {
+          card.classList.add('hidden')
+        }
+      })
+
+      e.target.parentElement.parentElement.classList.remove('closedCard')
+      e.target.parentElement.parentElement.classList.add('openCard')
+      setTimeout(function() {
+        e.target.parentElement.parentElement.lastElementChild.classList.remove('hidden')
+
+        setTimeout(function(){
+          e.target.parentElement.parentElement.lastElementChild.classList.add('opFull')
+        }, 250)
+      }, 750)
+    }
+  }
+
   // isolate source, call proper function
   if (e.target.classList.contains('card')) {
     handleParent(e)
   } else if (e.target.parentElement.classList.contains('card')) {
     handleChild(e)
+  } else if (e.target.parentElement.parentElement.classList.contains('card')) {
+    handleGrandchild(e)
   }
 }
 
 // event listeners
 navToggle.addEventListener('click', e => toggleNavState(e))
 aboutCardsArea.addEventListener('click', e => toggleAboutCardState(e))
+navLinkContainer.addEventListener('click', e => handleNavLink(e))
 
 // init page JS
 init()
